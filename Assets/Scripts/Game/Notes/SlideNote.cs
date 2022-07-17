4f7e0c928dc180e636247499f08e28e897181e58
Kurante <kurante2801@gmail.com>
2022-07-17 17:16:56 -0300

    Tweak slide notes

--- Assets/Scripts/Game/Notes/SlideNote.cs
+++ Assets/Scripts/Game/Notes/SlideNote.cs
@@ -40,7 +40,7 @@ public class SlideNote : Note
         float y = Mathf.Max(0f, GetPosition(time, Model));
         transform.localPosition = new Vector3(0f, y, 0f);
 
-        if ((difference < NoteGradeExtensions.Timings[(int)NoteGrade.Good] && Track.Fingers.Count > 0) || (IsAuto && difference <= 0) || difference < -NoteGradeExtensions.Timings[(int)NoteGrade.Good])
+        if ((difference < NoteGradeExtensions.Timings[(int)NoteGrade.Perfect] && Track.Fingers.Count > 0) || (IsAuto && difference <= 0) || difference < -NoteGradeExtensions.Timings[(int)NoteGrade.Good])
             JudgeNote(time);
                 
     }
@@ -69,4 +69,5 @@ public class SlideNote : Note
         }
     }
 
+    public override void OnTrackDown(int time) => JudgeNote(time);
 }
